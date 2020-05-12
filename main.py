94c091847a338f85bcc3c5ae3bb189b88c775796
safarmammadov <safarmobile@gmail.com>
2020-05-12 20:48:26 +0300

    translation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from translate import Translator
 translator= Translator(to_lang="zh")
 print ('Фира прошу внеси слова на английском языке')
 WORD = (input())
+
 print (translator.translate(WORD))
 
 
